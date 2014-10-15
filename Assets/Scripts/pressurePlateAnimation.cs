635dc2490ec05905fb9ce606c972bf5c92bed9c8
Bueno Lucchese, Iago <iagobuen@indiana.edu>
2014-10-15 10:47:42 -0400

    further work done on water room added pressure plates started work on glass cube, inventory system

---
+++ Assets/Scripts/pressurePlateAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressurePlateAnimation : MonoBehaviour {
+
+	public GameObject triggeredBy;
+	public float lengthOfAnimation = 1f;
+	public float sinkAmount = 0.09f;
+
+	private float timeElapsed = float.PositiveInfinity;
+	private bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (timeElapsed <= lengthOfAnimation) {
+			timeElapsed += Time.deltaTime;
+
+			transform.position -= new Vector3(0,Mathf.Lerp(0,sinkAmount*Time.deltaTime,timeElapsed/lengthOfAnimation),0); //shifts the plate in the Y axis
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.Equals(triggeredBy) && !triggered) {
+			timeElapsed = 0;
+			triggered = true;
+		}
+	}
+}
